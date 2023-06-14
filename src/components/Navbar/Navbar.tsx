b166722f5d49c73c35c1ca1a6b5133e0989d866a
Evan House <evanahouse@gmail.com>
2023-06-13 23:01:25 -0700

    put hamburger button on right side of the navbar in mobile

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -87,7 +87,14 @@ function ResponsiveAppBar() {
           </Box>
 
           {/* MOBILE VIEW */}
-          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+          <Box
+            sx={{
+              flexGrow: 1,
+              //show on right side not left
+              justifyContent: "end",
+              display: { xs: "flex", md: "none" },
+            }}
+          >
             <IconButton
               size="large"
               aria-label="account of current user"
@@ -117,10 +124,13 @@ function ResponsiveAppBar() {
               }}
             >
               {pages.map((page) => (
-                <MenuItem key={page}  onClick={() => {
-                  handleCloseNavMenu();
-                  navigate(getPageURL(page));
-                }}>
+                <MenuItem
+                  key={page}
+                  onClick={() => {
+                    handleCloseNavMenu();
+                    navigate(getPageURL(page));
+                  }}
+                >
                   <Typography textAlign="center">{page}</Typography>
                 </MenuItem>
               ))}
