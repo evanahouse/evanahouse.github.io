790ef02bb4029addb0097746f3462e0357d95704
Evan House <evanahouse@gmail.com>
2023-06-20 14:29:31 -0700

    added buttons to contact page

--- src/components/Contact/components/Footer.tsx
+++ src/components/Contact/components/Footer.tsx
@@ -5,9 +5,10 @@ import LinkedInIcon from "@mui/icons-material/LinkedIn";
 
 import InstagramIcon from "@mui/icons-material/Instagram";
 import { Container, Typography } from "@mui/material";
+import { BsMedium } from "react-icons/bs";
+import { Link } from "react-router-dom";
 import SocialButton from "../../../common/SocialButton";
 import useIsMobile from "../../../hooks/useIsMobile";
-
 
 const Footer = () => {
   const { isMobile } = useIsMobile();
@@ -26,7 +27,7 @@ const Footer = () => {
         sx={{
           display: "flex",
           flexDirection: "row",
-          justifyContent: "flex-start",
+          justifyContent: "center",
           alignSelf: "center",
           alignItems: "center",
           pb: 2,
@@ -55,10 +56,11 @@ const Footer = () => {
         sx={{
           display: "flex",
           flexDirection: "row",
-          justifyContent: "flex-start",
+          justifyContent: "center",
           alignSelf: "center",
           alignItems: "center",
           pb: 2,
+          pt: 2,
         }}
       >
         <SocialButton
@@ -85,46 +87,74 @@ const Footer = () => {
         sx={{
           display: "flex",
           flexDirection: "row",
-          justifyContent: "flex-start",
+          justifyContent: "center",
           alignSelf: "center",
           alignItems: "center",
+          mt: 2,
         }}
       >
-        <SocialButton
-          onClick={() => {}}
-          sx={{
-            width: isMobile ? 50 : 75,
-            height: isMobile ? 50 : 75,
-          }}
+        <Link
+          to="https://www.linkedin.com/in/evanahouse/"
+          target="_blank"
+          rel="noopener noreferrer"
+          style={{ textDecoration: "none" }}
         >
-          <LinkedInIcon sx={{ fontSize: isMobile ? 30 : 40 }} />
-        </SocialButton>
-        <SocialButton
-          onClick={() => {}}
-          sx={{
-            width: isMobile ? 50 : 75,
-            height: isMobile ? 50 : 75,
-          }}
+          <SocialButton
+            sx={{
+              width: isMobile ? 50 : 75,
+              height: isMobile ? 50 : 75,
+            }}
+          >
+            <LinkedInIcon sx={{ fontSize: isMobile ? 30 : 40 }} />
+          </SocialButton>
+          <Link
+            to="https://github.com/evanahouse"
+            target="_blank"
+            rel="noopener noreferrer"
+            style={{ textDecoration: "none" }}
+          >
+            <SocialButton
+              sx={{
+                width: isMobile ? 50 : 75,
+                height: isMobile ? 50 : 75,
+              }}
+            >
+              <GitHubIcon sx={{ fontSize: isMobile ? 30 : 40 }} />
+            </SocialButton>
+          </Link>
+        </Link>
+        <Link
+          to="https://evanahouse.medium.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+          style={{ textDecoration: "none" }}
         >
-          <GitHubIcon sx={{ fontSize: isMobile ? 30 : 40 }} />
-        </SocialButton>
-        <SocialButton
-          onClick={() => {}}
-          sx={{
-            width: isMobile ? 50 : 75,
-            height: isMobile ? 50 : 75,
-          }}
+          <SocialButton
+            onClick={() => {}}
+            sx={{
+              width: isMobile ? 50 : 75,
+              height: isMobile ? 50 : 75,
+            }}
+          >
+            <BsMedium size={isMobile ? 30 : 40} />
+          </SocialButton>
+        </Link>
+        <Link
+          to="https://www.instagram.com/evanahouse/"
+          target="_blank"
+          rel="noopener noreferrer"
+          style={{ textDecoration: "none" }}
         >
-          <InstagramIcon sx={{ fontSize: isMobile ? 30 : 40 }} />
-        </SocialButton>
-        <SocialButton
-          onClick={() => {}}
-          sx={{
-            width: isMobile ? 50 : 75,
-            height: isMobile ? 50 : 75,
-          }}
-        >
-        </SocialButton>
+          <SocialButton
+            onClick={() => {}}
+            sx={{
+              width: isMobile ? 50 : 75,
+              height: isMobile ? 50 : 75,
+            }}
+          >
+            <InstagramIcon sx={{ fontSize: isMobile ? 30 : 40 }} />
+          </SocialButton>
+        </Link>
       </Container>
     </Container>
   );
