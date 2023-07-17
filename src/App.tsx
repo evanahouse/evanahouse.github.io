0ddbc07a5a9542b45422138f61dd13c7d3940d8f
Evan House <evanahouse@gmail.com>
2023-07-17 12:05:19 -0700

    revert back to router

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import { ThemeProvider } from "@mui/material/styles";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Contact from "./components/Contact/Contact";
 import Home from "./components/Home/Home";
@@ -9,19 +9,20 @@ import Resume from "./components/Resume/Resume";
 import ThankYou from "./components/ThankYou/ThankYou";
 import "./fonts.css";
 import theme from "./theme";
+import { updatePropertyAccessChain } from "typescript";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <BrowserRouter basename={process.env.PUBLIC_URL}>
         {/* <Navbar /> */}
-        <Switch>
+        <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/portfolio" element={<Portfolio />} />
           <Route path="/resume" element={<Resume />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/thankyou" element={<ThankYou />} />
-        </Switch>
+        </Routes>
       </BrowserRouter>
     </ThemeProvider>
   );
